f0e7f40acce66e106debd0c3f64a74ff5a9e5462
matt <mwall@csumb.edu>
2019-09-30 17:49:00 -0700

    grabbed first set of links

--- bs4GovDeals/govdeals.py
+++ bs4GovDeals/govdeals.py
@@ -2,25 +2,68 @@
 
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+domain = "https://www.govdeals.com/"
 URL = "https://www.govdeals.com/index.cfm?fa=Main.ZipSearch&zipcode=93905&miles=150&milesKilo=miles&category=00&kWordSelect=2&locationType=miles&kWord=&country=&btn_submit=Submit"
-r = requests.get(URL)
+
+def getLinks(url):
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    locations_table = soup.find('table', attrs={'class': 'searchResults'})
+    anchors = locations_table.findAll('a')
+    location_urls = [urljoin(domain, a['href']) for a in anchors]
+    # location_urls = [urljoin(str(a['href']), domain) for a in anchors]
+    return location_urls
+
+def getInfo(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    items_table = soup.find('table', attrs={'class': 'searchResults'})
+    item__rows = items_table.findAll('tr')
+    for row in item__rows:
+        anchors = row.findAll('a')
+        if anchors:
+            print('href', anchors[0].contents)
+            # print('photo', urljoin(domain, anchors[0].href))
+            # print('title', anchors[0].title)
+
+location_links = getLinks(URL)
+# print(location_links)
+
+for link in location_links:
+    getInfo(link)
+
+# r = requests.get(URL)
+#
+# soup = BeautifulSoup(r.content, 'html.parser')
+# # print(soup.prettify())
+#
+#
+# locations_table = soup.find('table', attrs={'class': 'searchResults'})
+# location_links = locations_table.findAll('a')
 
 
-soup = BeautifulSoup(r.content, 'html5lib')
-# print(soup.prettify())
 
-locations = []
 
-table = soup.find('table', attrs= {'class': 'searchResults'})
 
-for row in table.findAll('td'):
-    loc = {}
+# for location in location_links:
+#     location_url = urljoin(domain, location['href'])
+#     location_page = requests.get(location_url)
 
-    try:
-        loc['url'] = row.a['href']
 
-    except:
-        loc['url'] = 'None'
+# print(links)
 
-    locations.append(loc)
-print(locations)
+# for row in table.findAll('td'):
+#     loc = {}
+#
+#     try:
+#         loc['url'] = row.a['href']
+#
+#     except:
+#         loc['url'] = 'None'
+#
+#     locations.append(loc)
+# print(locations)
